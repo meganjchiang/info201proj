a34ea04409cf8822c5b37e8ce89dbd8cccea0e0c
meganjchiang <mjchiang@uw.edu>
2022-12-07 13:59:20 -0800

    add title and update footer

--- docs/Final/app_ui.R
+++ docs/Final/app_ui.R
@@ -19,16 +19,8 @@ source("tabs/tab_panel_report.R")
 
 
 ui <- navbarPage(
-  title = "Project Example",
+  title = "Education & Social Status in the United States",
   position = "fixed-top",
-
-  # A simple header
-  header = list(
-    tags$style(type = "text/css", "body {padding-top: 70px;}"),
-    hr(),
-    HTML("... Project Header... "),
-    hr()
-  ),
 
   # A simple footer
   footer = list(
